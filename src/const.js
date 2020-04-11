42f69efdbfdae39ecb9610400ce00c99e35ec3fe
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-11 20:17:07 +0300

    добавляет моки

---
+++ src/const.js
@@ -0,0 +1,34 @@
+const COLORS = [
+  `black`,
+  `yellow`,
+  `blue`,
+  `green`,
+  `pink`,
+];
+
+const DAYS = [
+  `mo`,
+  `tu`,
+  `we`,
+  `th`,
+  `fr`,
+  `sa`,
+  `su`,
+];
+
+const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `Jule`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export {COLORS, DAYS, MONTH_NAMES};
