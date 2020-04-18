c588eb2adab6395390d987c1030ef92bcd4ea5d0
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-19 00:45:00 +0300

    4.1.10 Использует новые функции для отрисовки задач

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,5 @@
-import {formatTime} from "../utils.js";
-import {createElement, MONTH_NAMES} from "../const.js";
+import {createElement, formatTime} from "../utils.js";
+import {MONTH_NAMES} from "../const.js";
 
 const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
--- src/main.js
+++ src/main.js
@@ -17,12 +17,12 @@ const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const renderTask = (taskListElement, task) => {
   const onEditButtonClick = () => {
-    taskListElement.replaceChild(TaskEditComponent.getElement(), TaskComponent.getElement());
+    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
   };
 
   const onEditFormSubmit = (evt) => {
     evt.preventDefault();
-    taskListElement.replaceChild(TaskComponent.getElement(), TaskEditComponent.getElement());
+    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
   };
 
   const taskComponent = new TaskComponent(task);
@@ -30,7 +30,7 @@ const renderTask = (taskListElement, task) => {
   editButton.addEventListener(`click`, onEditButtonClick);
 
   const taskEditComponent = new TaskEditComponent(task);
-  const editForm = taskEditComponent.getElement().querySelector('form');
+  const editForm = taskEditComponent.getElement().querySelector(`form`);
   editForm.addEventListener(`submit`, onEditFormSubmit);
 
   render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
@@ -38,15 +38,17 @@ const renderTask = (taskListElement, task) => {
 
 const renderBoard = (boardComponent, tasks) => {
   render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
-  render(boardComponent.getElement(), new TasksComponent().getElement(). RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
 
   const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
 
   let showingTaskCount = SHOWING_TASKS_COUNT_ON_START;
   tasks.slice(0, showingTaskCount)
-    .foreach((task) => renderTask(taskListElement, task));
+    .forEach((task) => {
+      renderTask(taskListElement, task);
+    });
 
-  const loadMoreButtonComponent = new LoadMoreButtonComponent().getElement();
+  const loadMoreButtonComponent = new LoadMoreButtonComponent();
   render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
   loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
@@ -54,14 +56,13 @@ const renderBoard = (boardComponent, tasks) => {
     showingTaskCount = showingTaskCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
     tasks.slice(prevTaskCount, showingTaskCount)
-      .foreach((task) => renderTask(taskListElement, task));
+      .forEach((task) => renderTask(taskListElement, task));
 
     if (showingTaskCount >= tasks.length) {
       loadMoreButtonComponent.getElement().remove();
-      loadMoreButtonComponent.remoeElement();
+      loadMoreButtonComponent.removeElement();
     }
   });
-
 
 };
 
@@ -74,3 +75,7 @@ const tasks = generateTasks(TASK_COUNT);
 
 render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
+
+const boardComponent = new BoardComponent();
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+renderBoard(boardComponent, tasks);
