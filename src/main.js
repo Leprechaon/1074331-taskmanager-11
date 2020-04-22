0f8738afe7c6da30971f0afb354887c91e795dd6
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-23 01:22:11 +0300

    5.1.13 Добавит в компонент load-more метод для подписки на клик

--- src/main.js
+++ src/main.js
@@ -74,7 +74,7 @@ const renderBoard = (boardComponent, tasks) => {
   const loadMoreButtonComponent = new LoadMoreButtonComponent();
   render(boardComponent.getElement(), loadMoreButtonComponent, RenderPosition.BEFOREEND);
 
-  loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
+  loadMoreButtonComponent.setClickHandler(() => {
     const prevTaskCount = showingTaskCount;
     showingTaskCount = showingTaskCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
