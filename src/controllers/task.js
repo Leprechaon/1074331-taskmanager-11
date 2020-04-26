7aa3adc40ca87015201b713d8c874a9a30089d94
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-26 23:14:01 +0300

    6.1.1 Создаст контроллер задачи

---
+++ src/controllers/task.js
@@ -0,0 +1,13 @@
+import TaskComponent from "../components/task.js";
+import TaskEditComponent from "../components/task-edit.js";
+import {render, replace, RenderPosition} from "../utils/render.js";
+
+export default class TaskController {
+  constructor(container) {
+
+  }
+
+  render(task) {
+
+  }
+}
